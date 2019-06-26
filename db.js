e6f99597e881496c3474f061f0cb01413e74b5d2
Hans Martin <hansmhank@gmail.com>
2019-06-25 17:15:57 -0700

    Some stuff

--- db.js
+++ db.js
@@ -1,6 +1,13 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
+console.log({
+  socketPath: process.env.DATABASE_SOCKET,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
 const connection = mysql.createConnection({
   socketPath: process.env.DATABASE_SOCKET,
   user: process.env.USER,
